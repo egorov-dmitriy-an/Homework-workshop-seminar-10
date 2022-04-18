6631a0138ed382baecfb49c1cb32eb25110414c7
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-18 19:09:43 +0300

    Add Seminar 10 Task 1

--- Seminar 10 Task 1/Program.cs
+++ Seminar 10 Task 1/Program.cs
@@ -1,3 +1,35 @@
 ﻿// Задача 1.Дано число n. Получите число, записанное в обратном порядке.
 // 345 >> 543
 // 81 >> 18
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("=============== Семинар 10 Задача 1 ==================");
+            Console.Write("Введмите исходное число:  ");
+            string number = Console.ReadLine();
+
+            string Reverse(string tempNumber, int index)
+            {
+                string revNumber = string.Empty;
+                while (index < tempNumber.Length)
+                {
+                    revNumber = tempNumber[index] + revNumber;
+                    index++;
+                    //Console.WriteLine(revNumber);
+                    Reverse(tempNumber, index);
+                }
+                return revNumber;
+            }
+            Console.WriteLine("Число в обратном порядке: " + Reverse(number, 0));
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
