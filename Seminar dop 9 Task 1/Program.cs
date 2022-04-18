af52c3e284f26599f1c9d2b62e36aac67a47d90f
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-19 00:58:09 +0300

    Add Seminar dop 9 Task 1

--- Seminar dop 9 Task 1/Program.cs
+++ Seminar dop 9 Task 1/Program.cs
@@ -1,3 +1,39 @@
 ﻿// Задача 1. Дано предложение. Напишите рекурсивный метод,
 // подсчитывающий количество слов в данном предложении.
 // Словом считается последовательность символов без пробелов.
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 9 Задача 1 доп =============");
+            int slovo = 0;
+            int Word(string methodText, int count, int index)
+            {
+                if (index < methodText.Length)
+                {
+                    if (methodText[index] == ' ')
+                    {
+                        count++;
+                    }
+                    index++;
+                    slovo = count;
+                    Word(methodText, count, index);
+                }
+                return slovo;
+            }
+
+            Console.WriteLine("Введите предложение: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Количество слов в предложении: " + Word(text, 1, 0));
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
